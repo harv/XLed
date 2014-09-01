ca758f6b1f657bf1fd15a5433ab0ec8007d564bb
cinhoo <ch05042210@gmail.com>
2014-09-01 10:08:10 +0800

    restored to previous notification led hook point

--- src/com/haoutil/xposed/xled/XposedMod.java
+++ src/com/haoutil/xposed/xled/XposedMod.java
@@ -1,6 +1,7 @@
 package com.haoutil.xposed.xled;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -27,51 +28,52 @@ public class XposedMod implements IXposedHookZygoteInit {
         settingsHelper = new SettingsHelper();
 
         // notification led
-        XposedHelpers.findAndHookMethod("com.android.server.NotificationManagerService", null, "enqueueNotificationWithTag",
-                String.class, String.class, int.class, Notification.class, int[].class, int.class, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        settingsHelper.reload();
+        XC_MethodHook hook = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                settingsHelper.reload();
 
-                        if (!settingsHelper.getBoolean("pref_enable", true)) {
-                            return;
-                        }
+                if (!settingsHelper.getBoolean("pref_enable", true)) {
+                    return;
+                }
 
-                        String packageName = (String) param.args[0];
-                        if (packageName.equals("com.haoutil.xposed.xled") || !settingsHelper.getBoolean("pref_app_enable_" + packageName, false)) {
-                            return;
-                        }
+                String packageName = ((Context) XposedHelpers.getObjectField(param.thisObject, "mContext")).getPackageName();
+                if (packageName.equals("com.haoutil.xposed.xled") || !settingsHelper.getBoolean("pref_app_enable_" + packageName, false)) {
+                    return;
+                }
 
-                        Logger.log(TAG, "handle package " + packageName);
+                Logger.log(TAG, "handle package " + packageName);
 
-                        Notification notification = (Notification) param.args[3];
-                        if ((notification.defaults & Notification.DEFAULT_LIGHTS) == Notification.DEFAULT_LIGHTS) {
-                            Logger.log(TAG, "ignore default led settings(Notification.DEFAULT_LIGHTS).");
-                            notification.defaults &= ~(~notification.defaults | Notification.DEFAULT_LIGHTS);
-                            notification.flags |= Notification.FLAG_SHOW_LIGHTS;
-                        }
+                Notification notification = (Notification) param.args[2];
+                if ((notification.defaults & Notification.DEFAULT_LIGHTS) == Notification.DEFAULT_LIGHTS) {
+                    Logger.log(TAG, "ignore default led settings(Notification.DEFAULT_LIGHTS).");
+                    notification.defaults &= ~(~notification.defaults | Notification.DEFAULT_LIGHTS);
+                    notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+                }
 
-                        if (settingsHelper.getBoolean("pref_app_disableled_" + packageName, false)) {
-                            Logger.log(TAG, "disable led flashing.");
-                            notification.flags &= ~(~notification.flags | Notification.FLAG_SHOW_LIGHTS);
-                            return;
-                        }
+                if (settingsHelper.getBoolean("pref_app_disableled_" + packageName, false)) {
+                    Logger.log(TAG, "disable led flashing.");
+                    notification.flags &= ~(~notification.flags | Notification.FLAG_SHOW_LIGHTS);
+                    return;
+                }
 
-                        if ((notification.flags & Notification.FLAG_SHOW_LIGHTS) != Notification.FLAG_SHOW_LIGHTS
-                                && settingsHelper.getBoolean("pref_app_forceenable_" + packageName, true)) {
-                            Logger.log(TAG, "force led flashing.");
-                            notification.flags |= Notification.FLAG_SHOW_LIGHTS;
-                        }
+                if ((notification.flags & Notification.FLAG_SHOW_LIGHTS) != Notification.FLAG_SHOW_LIGHTS
+                        && settingsHelper.getBoolean("pref_app_forceenable_" + packageName, true)) {
+                    Logger.log(TAG, "force led flashing.");
+                    notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+                }
 
-                        Logger.log(TAG, "changing led settings... {color:" + notification.ledARGB + ",onms:" + notification.ledOnMS + ",offms:" + notification.ledOffMS + "}");
-                        notification.ledARGB = settingsHelper.getInt("pref_app_color_" + packageName, Color.TRANSPARENT);
-                        notification.ledOnMS = settingsHelper.getInt("pref_app_onms_" + packageName, settingsHelper.getInt("pref_led_onms", 300));
-                        notification.ledOffMS = settingsHelper.getInt("pref_app_offms_" + packageName, settingsHelper.getInt("pref_led_offms", 1000));
-                        Logger.log(TAG, "change led settings succeed {color:" + notification.ledARGB + ",onms:" + notification.ledOnMS + ",offms:" + notification.ledOffMS + "}");
+                Logger.log(TAG, "changing led settings... {color:" + notification.ledARGB + ",onms:" + notification.ledOnMS + ",offms:" + notification.ledOffMS + "}");
+                notification.ledARGB = settingsHelper.getInt("pref_app_color_" + packageName, Color.TRANSPARENT);
+                notification.ledOnMS = settingsHelper.getInt("pref_app_onms_" + packageName, settingsHelper.getInt("pref_led_onms", 300));
+                notification.ledOffMS = settingsHelper.getInt("pref_app_offms_" + packageName, settingsHelper.getInt("pref_led_offms", 1000));
+                Logger.log(TAG, "change led settings succeed {color:" + notification.ledARGB + ",onms:" + notification.ledOnMS + ",offms:" + notification.ledOffMS + "}");
 
-                        param.args[3] = notification;
-                    }
-                });
+                param.args[2] = notification;
+            }
+        };
+        XposedHelpers.findAndHookMethod(NotificationManager.class, "notify", new Object[] {String.class, Integer.TYPE, Notification.class, hook});
+        XposedHelpers.findAndHookMethod(NotificationManager.class, "notifyAsUser", new Object[] {String.class, Integer.TYPE, Notification.class, "android.os.UserHandle", hook});
 
         // screen on led
         XposedHelpers.findAndHookMethod("com.android.server.NotificationManagerService", null, "systemReady", new XC_MethodHook() {
